6736a3fc60ecb901bfd90b8eae08fa25bf42f705
Halley <halleydao2004@gmail.com>
2025-05-09 04:03:12 +0000

    implement ab pruning and update eval

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,5 +1,6 @@
 # COMP30024 Artificial Intelligence, Semester 1 2025
 # Project Part B: Game Playing Agent
 
-from .program import Agent
 from .minimaxAgent import MinimaxAgent
+from .abAgent import ABAgent
+from .program import Agent
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-python -m referee -d -l "log.txt" agent:MinimaxAgent agent:Agent
+# python -m referee -d -l "log.txt" agent:MinimaxAgent agent:Agent 
+python -m referee -d -l "log.txt" agent:ABAgent agent:Agent
